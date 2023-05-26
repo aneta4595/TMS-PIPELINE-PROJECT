715ed016a6348bb723b25978055e20257d159ccf
Aneta Walicka <aneta4595@wp.pl>
2023-05-26 17:30:09 +0200

    small changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TeamsPageComponent } from './components/teams-page/teams-page.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { TeamsPageComponentModule } from './components/teams-page/teams-page.component-module';
+import { ProjectsComponentModule } from './components/projects/projects.component-module';
 
-const routes: Routes = [{ path: 'teams', component: TeamsPageComponent }];
+const routes: Routes = [
+  { path: 'teams', component: TeamsPageComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), TeamsPageComponentModule],
+  imports: [
+    RouterModule.forRoot(routes),
+    TeamsPageComponentModule,
+    ProjectsComponentModule,
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
