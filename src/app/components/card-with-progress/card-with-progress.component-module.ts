26928de7ae6f72ee39972c7bbc32ee38da117f4c
Aneta Walicka <aneta4595@wp.pl>
2023-05-26 17:27:17 +0200

    card-with-progress-bar created

---
+++ src/app/components/card-with-progress/card-with-progress.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CardWithProgressComponent } from './card-with-progress.component';
+
+@NgModule({
+  imports: [],
+  declarations: [CardWithProgressComponent],
+  providers: [],
+  exports: [CardWithProgressComponent]
+})
+export class CardWithProgressComponentModule {
+}
