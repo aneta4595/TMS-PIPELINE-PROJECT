3de735f3b2d4f6f438e8c214259f66539346f1fa
Aneta Walicka <aneta4595@wp.pl>
2023-05-26 17:29:22 +0200

    team model fixed

--- src/app/models/team.model.ts
+++ src/app/models/team.model.ts
@@ -1,7 +1,21 @@
 export interface TeamModel {
   readonly name: string;
   readonly description: string;
-  readonly members: string[];
-  readonly projects: string[];
+  readonly members: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    position: string;
+    avatarUrl: string;
+  }[];
+  readonly projects: {
+    id: string;
+    name: string;
+    description: string;
+    startDate: string;
+    dueDate: string;
+    visibility: string;
+    assignees: string[];
+  }[];
   readonly id: string;
 }
