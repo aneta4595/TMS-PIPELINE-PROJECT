c5ed92b01d80ee0a4431155cf7fc47fa52786728
Aneta Walicka <aneta4595@wp.pl>
2023-05-26 17:29:57 +0200

    change api in service

--- src/app/services/teams.service.ts
+++ src/app/services/teams.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TeamModel } from '../models/team.model';
+import { ProjectModel } from '../models/project.model';
 
 @Injectable({ providedIn: 'root' })
 export class TeamsService {
@@ -9,7 +10,13 @@ export class TeamsService {
 
   getAllTeams(): Observable<TeamModel[]> {
     return this._httpClient.get<TeamModel[]>(
-      'https://6384fca14ce192ac60696c4b.mockapi.io/teams'
+      'https://646f899709ff19b120876fb8.mockapi.io/teams'
+    );
+  }
+
+  getAllProjects(): Observable<ProjectModel[]> {
+    return this._httpClient.get<ProjectModel[]>(
+      'https://646f899709ff19b120876fb8.mockapi.io/projects'
     );
   }
 }
