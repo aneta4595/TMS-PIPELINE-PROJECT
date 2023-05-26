7b5b3568bff506c7c59e7877bd8425bc7cef25c9
Aneta Walicka <aneta4595@wp.pl>
2023-05-26 17:29:32 +0200

    project model created

---
+++ src/app/models/project.model.ts
@@ -0,0 +1,10 @@
+export interface ProjectModel {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly startDate: string;
+  readonly dueDate: string;
+  readonly visibility: string;
+  readonly percentage: number;
+  readonly assignees: string[];
+}
