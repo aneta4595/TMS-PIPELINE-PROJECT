f01237b8ad7287224ace90bc1fdeb877183fb378
Aneta Walicka <aneta4595@wp.pl>
2023-05-31 23:29:22 +0200

    delete useless things

--- src/app/components/card-with-avatar-and-checklist/card-with-avatar-and-checklist.component-module.ts
+++ src/app/components/card-with-avatar-and-checklist/card-with-avatar-and-checklist.component-module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CardWithAvatarAndChecklistComponent } from './card-with-avatar-and-checklist.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [CardWithAvatarAndChecklistComponent],
   providers: [],
-  exports: [CardWithAvatarAndChecklistComponent]
+  exports: [CardWithAvatarAndChecklistComponent],
 })
-export class CardWithAvatarAndChecklistComponentModule {
-}
+export class CardWithAvatarAndChecklistComponentModule {}
