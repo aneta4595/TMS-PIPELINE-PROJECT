18ac86d5fa4633a715622aa9d66a7ad38385beab
Aneta Walicka <aneta4595@wp.pl>
2023-06-01 12:49:59 +0200

    hamburger menu added

--- src/app/components/navigation/navigation.component-module.ts
+++ src/app/components/navigation/navigation.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NavigationComponent } from './navigation.component';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   declarations: [NavigationComponent],
   providers: [],
   exports: [NavigationComponent],
--- src/app/components/test/test.component-module.ts
+++ src/app/components/test/test.component-module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { TestComponent } from './test.component';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule, RouterModule],
   declarations: [TestComponent],
   providers: [],
-  exports: [TestComponent]
+  exports: [TestComponent],
 })
-export class TestComponentModule {
-}
+export class TestComponentModule {}
